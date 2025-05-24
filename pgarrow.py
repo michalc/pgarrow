77b6f43ac80520382a3f83e06f21b0ee6d887f14
Michal Charemza <michal@charemza.name>
2025-05-24 15:45:23 +0100

    feat: register dialect

---
+++ pgarrow.py
@@ -0,0 +1,7 @@
+from sqlalchemy.dialects.postgresql.base import PGDialect
+
+
+class PgDialect_pgarrow(PGDialect):
+    @classmethod
+    def import_dbapi(cls):
+        pass
---
+++ test_pgarrow.py
@@ -0,0 +1,4 @@
+import sqlalchemy as sa
+
+def test_initialise():
+	engine = sa.create_engine('postgresql+pgarrow://postgres@127.0.0.1:5432/')
