0cc13112a7e9cbd690299d6ec22250c895f2396a
Michal Charemza <michal@charemza.name>
2025-05-26 05:22:41 +0100

    feat: ability to run basic queries

--- test_pgarrow.py
+++ test_pgarrow.py
@@ -1,5 +1,25 @@
+import uuid
+
+import pytest
 import sqlalchemy as sa
 
 
-def test_initialise():
-	engine = sa.create_engine('postgresql+pgarrow://postgres@127.0.0.1:5432/')
+def test_trivial_query():
+    engine = sa.create_engine('postgresql+pgarrow://postgres:password@127.0.0.1:5432/')
+    with engine.connect() as conn:
+        assert conn.execute(sa.text("SELECT 1")).fetchall() == [(1,)]
+
+
+def test_basic_transaction_isolation():
+    engine = sa.create_engine('postgresql+pgarrow://postgres:password@127.0.0.1:5432/')
+    table_name = "table_" + uuid.uuid4().hex
+
+    with \
+            engine.connect() as conn_1, \
+            engine.connect() as conn_2:
+
+        conn_1.execute(sa.text(f"CREATE TABLE {table_name} (id int)"))
+        conn_1.execute(sa.text(f"INSERT INTO {table_name} VALUES (1)"))
+
+        with pytest.raises(sa.exc.ProgrammingError, match=f'relation "{table_name}" does not exist'):
+            assert conn_2.execute(sa.text(f"SELECT * FROM {table_name}")).fetchall() == [(1,)]
